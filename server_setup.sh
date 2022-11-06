c8157d12850cf74dd40f523b20df2728eec93c58
yuito723 <102805748+yuito723@users.noreply.github.com>
2022-11-06 19:16:02 +0900

    Update server_setup.sh

--- server_setup.sh
+++ server_setup.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
 sudo apt -y update&&sudo apt -y upgrade&&sudo apt -y autoremove&&
-sudo apt -y install xserver-xorg raspberrypi-ui-mods nano htop apache2 samba python3-pip&&
+sudo apt -y install xserver-xorg raspberrypi-ui-mods nano htop apache2 samba python3-pip ffmpeg&&
+sudo pip3 install yt-dlp
 sudo shutdown -r now
