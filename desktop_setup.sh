6ef1417f23d809c2fb2525336565e506d553b964
yuito723 <102805748+yuito723@users.noreply.github.com>
2022-11-06 19:17:29 +0900

    Update desktop_setup.sh

--- desktop_setup.sh
+++ desktop_setup.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/bash
-sudo apt -y update&&sudo apt -y upgrade&&sudo apt -y autoremove
+sudo apt -y update&&sudo apt -y upgrade&&sudo apt -y autoremove&&
+sudo apt -y nano htop rpi-imager python3-pip ffmpeg&&
+sudo pip3 install yt-dlp
+sudo shutdown -r now
