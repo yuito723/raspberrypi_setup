8553c1e81ec4f7e3c84a3f3634485edcf3f82fdb
yuito723 <102805748+yuito723@users.noreply.github.com>
2022-11-06 19:12:29 +0900

    Update server_setup.sh

--- server_setup.sh
+++ server_setup.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
-sudo apt -y update&&sudo apt -y upgrade&&sudo apt -y autoremove\
-sudo apt -y install xserver-xorg raspberrypi-ui-mods nano htop apache2 samba python3-pip\
+sudo apt -y update&&sudo apt -y upgrade&&sudo apt -y autoremove&&
+sudo apt -y install xserver-xorg raspberrypi-ui-mods nano htop apache2 samba python3-pip&&
+sudo shutdown -r now
